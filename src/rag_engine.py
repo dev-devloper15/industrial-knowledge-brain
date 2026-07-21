15f892447921e392bf7c26ad64f9da91ec680506
Dev Tiwari <2k24mcadt@gmail.com>
2026-07-21 23:40:38 +0530

    Update rag_engine.py

--- src/rag_engine.py
+++ src/rag_engine.py
@@ -5,49 +5,44 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 
-# 1. Environment variables load karna
 load_dotenv()
 
 DATA_DIR = "data"
 CHROMA_DIR = "chroma_db"
 
 def initialize_knowledge_base():
-    """PDFs ko padh kar local vector store banane ka function"""
-    # Agar data folder nahi hai toh banao
     if not os.path.exists(DATA_DIR):
         os.makedirs(DATA_DIR)
-        print(f"📁 '{DATA_DIR}' folder bana diya gaya hai. Isme apni PDFs daalein.")
+        print(f"📁 '{DATA_DIR}' folder is created. insert PDFs.")
         return None
 
-    # Folder khali hone par warning
     if not os.listdir(DATA_DIR):
-        print(f"⚠️ Warning: '{DATA_DIR}' folder khali hai! PDFs daalna zaroori hai.")
+        print(f"⚠️ Warning: '{DATA_DIR}' folder is empty please insert pdf file")
         return None
 
-    print("📄 PDFs ko read kiya jaa raha hai...")
+    print("📄 PDFs read is in process")
     loader = PyPDFDirectoryLoader(DATA_DIR)
     docs = loader.load()
-    print(f"📚 Total {len(docs)} pages load hue.")
+    print(f"📚 Total {len(docs)} pages load")
 
-    print("✂️ Text ko chunks me toda jaa raha hai...")
+    print("✂️ Text break into chunks")
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     final_docs = text_splitter.split_documents(docs)
-    print(f"🧩 Total {len(final_docs)} chunks banaye gaye.")
+    print(f"🧩 Total {len(final_docs)} chunks")
 
-    print("🧠 Local HuggingFace Engine se Vector Database taiyar ho raha hai...")
+    print("🧠 Local HuggingFace Engine to Vector Database going to start")
     
-    # 📌 100% Stable Local Model - API Key ki koi zaroorat nahi hai
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     
-    # ChromaDB directory local disk par generate hogi
+    # ChromaDB directory generated on local disk 
     vectorstore = Chroma.from_documents(
         documents=final_docs, 
         embedding=embeddings, 
         persist_directory=CHROMA_DIR
     )
-    print("💾 ChromaDB local disk par safely save ho gaya!")
+    print("💾 ChromaDB safely save!")
     return vectorstore
 
 if __name__ == "__main__":
-    print("🚀 Fresh Initialization Test Shuru...")
+    print("🚀 Fresh Initialization Test started")
     initialize_knowledge_base()
